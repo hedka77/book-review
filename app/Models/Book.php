edb3490fa1f9751f64e2e1371513b96857f347a8
Edgar S. Ochoa <eserrano@sinprol.com.mx>
2024-06-05 09:58:54 -0600

    avances

--- app/Models/Book.php
+++ app/Models/Book.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
@@ -12,5 +13,11 @@ class Book extends Model
     public function reviews()
     {
         return $this->hasMany(Review::class);
+    }
+    
+    public function scopeTitle(Builder $query, $title): Builder
+    {
+    
+    
     }
 }
