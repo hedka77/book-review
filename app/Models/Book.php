4cc44532d54c231f7452de0a32bfd18f24b74768
Edgar S. Ochoa <eserrano@sinprol.com.mx>
2024-06-05 18:04:46 -0600

    about to implement the first controller

--- app/Models/Book.php
+++ app/Models/Book.php
@@ -2,9 +2,10 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Query\Builder as QueryBuilder;
 
 class Book extends Model
 {
@@ -15,9 +16,39 @@ class Book extends Model
         return $this->hasMany(Review::class);
     }
     
-    public function scopeTitle(Builder $query, $title): Builder
+    public function scopeTitle(Builder $query, string $title): Builder
     {
+        return $query->where('title', 'LIKE', "%$title%");
+    }
+    
+    public function scopePopular(Builder $query, $from = null, $to = null): Builder|QueryBuilder
+    {
+        return $query->withCount([ 'reviews' => fn(Builder $q) => $this->dateRangeBuilder($q, $from, $to) ])
+            ->orderBy('reviews_count', 'DESC');
+    }
+    
+    public function scopeHighestRated(Builder $query, $from = null, $to = null): Builder|QueryBuilder
+    {
+        return $query->withAvg([ 'reviews' => fn(Builder $q) => $this->dateRangeBuilder($q, $from, $to) ], 'rating')
+            ->orderBy('reviews_avg_rating', 'DESC');
+    }
+    
+    public function scopeMinReviews(Builder $query, int $minReviews): Builder|QueryBuilder
+    {
+        return $query->having('reviews_count', '>=', $minReviews);
+    }
     
     
+    private function dateRangeBuilder(Builder $query, $from = null, $to = null)
+    {
+        if ($from && !$to) {
+            $query->where('created_at', '>=', $from);
+        }
+        elseif (!$from && $to) {
+            $query->where('created_at', '<=', $to);
+        }
+        elseif ($from && $to) {
+            $query->whereBetween('created_at', [ $from, $to ]);
+        }
     }
 }
